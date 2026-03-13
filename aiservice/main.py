a3b6ba463dace14eadeacb2b784919622c0c621f
sumit kale <kalesumit104@gmail.com>
2026-03-13 20:38:45 +0530

    Secure Python AI service with dotenv

--- aiservice/main.py
+++ aiservice/main.py
@@ -6,7 +6,10 @@ import io
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = FastAPI()
 
 # -----------------------------------------
